ec701c2306f2440be643049cc6b950cb971caf26
Du05-hi <duongchidu30072005@gmail.com>
2026-09-14 16:22:26 +0700

    Move zalo verifier file inside backend directory

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.responses import FileResponse
 from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -113,7 +114,7 @@ async def handle_zalo_message(request: Request):
     data = await request.json()
     print("Zalo Webhook Event:", data)
     return {"status": "success"}
-from fastapi.responses import FileResponse
+
 
 @app.get("/zalo_verifierN8Va4vkjNYj5djG8nBvW1GEwwIJp3DpD3Wn.html")
 async def zalo_verifier():
