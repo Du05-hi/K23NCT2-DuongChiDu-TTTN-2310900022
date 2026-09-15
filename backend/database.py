572672e122513b23c68b2096a215a46adb8099a5
Du05-hi <duongchidu30072005@gmail.com>
2026-09-15 17:16:17 +0700

    Hardcode MongoDB Atlas URI directly

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://duongchidu30072005_db_user:123456Du@cluster0.hj9fwnw.mongodb.net/?appName=Cluster0")
+# Gán trực tiếp chuỗi MongoDB Atlas Cloud
+MONGO_URI = "mongodb+srv://duongchidu30072005_db_user:123456Du@cluster0.hj9fwnw.mongodb.net/?appName=Cluster0"
 DB_NAME = os.getenv("MONGO_DB_NAME", "nguyen_trai_chatbot")
 
 try:
