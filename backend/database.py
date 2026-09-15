c82450e146d827551d60e66ed6509eaa859752da
Du05-hi <duongchidu30072005@gmail.com>
2026-09-15 17:40:40 +0700

    Final fix for MongoDB SSL handshake on Render

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,22 @@
 import os
+import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Chuỗi kết nối Replica Set chuẩn bỏ qua lỗi SSL Certificate trên Render
-MONGO_URI = (
-    "mongodb://duongchidu30072005_db_user:123456Du@"
-    "ac-urihfgo-shard-00-00.hj9fwnw.mongodb.net:27017,"
-    "ac-urihfgo-shard-00-01.hj9fwnw.mongodb.net:27017,"
-    "ac-urihfgo-shard-00-02.hj9fwnw.mongodb.net:27017/"
-    "nguyen_trai_chatbot?replicaSet=atlas-13c57a-shard-0&ssl=true&authSource=admin&tlsInsecure=true"
-)
-
-DB_NAME = "nguyen_trai_chatbot"
+MONGO_URI = "mongodb+srv://duongchidu30072005_db_user:123456Du@cluster0.hj9fwnw.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = os.getenv("MONGO_DB_NAME", "nguyen_trai_chatbot")
 
 try:
+    # Truyền trực tiếp các tham số SSL bypass vào MongoClient
     client = MongoClient(
         MONGO_URI,
         serverSelectionTimeoutMS=5000,
-        connectTimeoutMS=5000
+        connectTimeoutMS=5000,
+        tls=True,
+        tlsAllowInvalidCertificates=True,
+        tlsCAFile=certifi.where()
     )
     db = client[DB_NAME]
     chat_history_collection = db["chat_history"]
