c2c966406bd5c6728702d88cfa58a4c325d22c5d
Du05-hi <duongchidu30072005@gmail.com>
2026-09-15 16:57:42 +0700

    Update MongoDB Atlas URI connection string

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://duongchidu30072005_db_user:123456Du@cluster0.hj9fwnw.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("MONGO_DB_NAME", "nguyen_trai_chatbot")
 
 client = MongoClient(MONGO_URI)
